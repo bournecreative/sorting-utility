7b19e6eb8fe207e89474884d78a16dff467fd535
Christian <bournecreative@gmail.com>
2024-06-11 13:54:42 -0500

    update sorter class to sort both number arrays and strings. Create type guards to allow us to select different sorting logic based on type

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 const numberSorted = new Sorter([100,34,5,59,4,23]);
+const stringSorted = new Sorter('LukeIAmYourFather');
 const nums = numberSorted.sortCollection()
+const sortStrs = stringSorted.sortCollection()
 console.log(numberSorted.collection)
+console.log(stringSorted.collection)
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
--- src/sorter.ts
+++ src/sorter.ts
@@ -1,23 +1,63 @@
 export class Sorter {
 
-    collection: number[]
+    collection: number[] | string
 
-    constructor(collection: number[]) {
+    constructor(collection: number[] | string) {
         this.collection = collection
     }
 
     sortCollection():void {
         const length = this.collection.length
-        
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j+1]) {
-                    let temp = this.collection[j]
-                    this.collection[j] = this.collection[j+1]
-                    this.collection[j+1] = temp
+        if (this.collection instanceof Array) {
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j+1]) {
+                        let temp = this.collection[j]
+                        this.collection[j] = this.collection[j+1]
+                        this.collection[j+1] = temp
+                    }
+                }
+            }
+        }
+        if (typeof this.collection === "string") {
+            this.collection.split('')
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j+1]) {
+                        let temp = this.collection[j]
+                        this.collection[j] = this.collection[j+1]
+                        this.collection[j+1] = temp
+                    }
                 }
             }
         }
     }
     
 }
+
+/* Note for future. If you are doing a union with numbers and strings you wil come across this typescript error
+
+ collection: number[] | string
+
+Error: Index signature in type 'string | number[]' only permits reading.
+
+Typescript is telling us in its not so clear manner that we can get index values from strings just as we do with arrays but
+we can do assign index value like we do with arrays.
+
+In other words we can not assign values as shown below. This would work for arrays but not for string
+    let temp = this.collection[j]
+    this.collection[j] = this.collection[j+1]
+    this.collection[j] = this.collection[j+1]
+
+You need to use type guards when joining seperate but different types
+
+How to check if type is an array:
+
+if (this.collection instanceof Array) do this...
+
+
+How to check if type is a string. We use this method to check primitives
+
+if (typeof this.collection === "string") do this...
+
+*/
